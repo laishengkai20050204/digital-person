3a6eaebf0acba3b606cf3a6036cca130647abb69
laishengkai20050204 <48379994+laishengkai20050204@users.noreply.github.com>
2026-07-21 10:10:45 +0800

    refactor: use Lombok for LifeEvent accessors

--- src/main/java/com/laishengkai/digitalperson/experience/LifeEvent.java
+++ src/main/java/com/laishengkai/digitalperson/experience/LifeEvent.java
@@ -1,16 +1,19 @@
 package com.laishengkai.digitalperson.experience;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.util.Objects;
 
+@Getter
+@ToString
+@EqualsAndHashCode
 public final class LifeEvent {
 
     private final String description;
 
     public LifeEvent(String description) {
         this.description = Objects.requireNonNull(description, "description cannot be null");
-    }
-
-    public String getDescription() {
-        return description;
     }
 }
