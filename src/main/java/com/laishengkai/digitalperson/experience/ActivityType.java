1fe881ffc6066600ac09a62489fb5fa349e1e2b7
laishengkai20050204 <48379994+laishengkai20050204@users.noreply.github.com>
2026-07-21 11:05:09 +0800

    Assign concurrency mode to activity types

--- src/main/java/com/laishengkai/digitalperson/experience/ActivityType.java
+++ src/main/java/com/laishengkai/digitalperson/experience/ActivityType.java
@@ -1,16 +1,35 @@
 package com.laishengkai.digitalperson.experience;
 
+import lombok.Getter;
+
+@Getter
 public enum ActivityType {
-    STUDY,
-    WORK,
-    EAT,
-    SLEEP,
-    REST,
-    TRAVEL,
-    EXERCISE,
-    SOCIAL,
-    ENTERTAINMENT,
-    SHOPPING,
-    CHAT,
-    OTHER
+
+    STUDY(ActivityMode.EXCLUSIVE),
+    WORK(ActivityMode.EXCLUSIVE),
+    EAT(ActivityMode.EXCLUSIVE),
+    SLEEP(ActivityMode.EXCLUSIVE),
+    REST(ActivityMode.EXCLUSIVE),
+    TRAVEL(ActivityMode.EXCLUSIVE),
+    EXERCISE(ActivityMode.EXCLUSIVE),
+    SOCIAL(ActivityMode.EXCLUSIVE),
+    ENTERTAINMENT(ActivityMode.EXCLUSIVE),
+    SHOPPING(ActivityMode.EXCLUSIVE),
+    CHAT(ActivityMode.CONCURRENT),
+    LISTEN_MUSIC(ActivityMode.CONCURRENT),
+    OTHER(ActivityMode.EXCLUSIVE);
+
+    private final ActivityMode mode;
+
+    ActivityType(ActivityMode mode) {
+        this.mode = mode;
+    }
+
+    public boolean isExclusive() {
+        return mode == ActivityMode.EXCLUSIVE;
+    }
+
+    public boolean isConcurrent() {
+        return mode == ActivityMode.CONCURRENT;
+    }
 }
