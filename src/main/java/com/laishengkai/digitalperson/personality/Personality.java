2aa70aff199e8708204b33d0e27b52b5d7af5d3f
laishengkai20050204 <48379994+laishengkai20050204@users.noreply.github.com>
2026-07-21 10:10:07 +0800

    refactor: use Lombok for Personality accessors

--- src/main/java/com/laishengkai/digitalperson/personality/Personality.java
+++ src/main/java/com/laishengkai/digitalperson/personality/Personality.java
@@ -1,10 +1,17 @@
 package com.laishengkai.digitalperson.personality;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
 /**
  * A person's stable personality traits based on the HEXACO model.
  *
  * <p>Every dimension is represented by a value from {@code 0.0} to {@code 1.0}.
  */
+@Getter
+@ToString
+@EqualsAndHashCode
 public final class Personality {
 
     private final double honestyHumility;
@@ -28,30 +35,6 @@ public final class Personality {
         this.agreeableness = validate("agreeableness", agreeableness);
         this.conscientiousness = validate("conscientiousness", conscientiousness);
         this.openness = validate("openness", openness);
-    }
-
-    public double getHonestyHumility() {
-        return honestyHumility;
-    }
-
-    public double getEmotionality() {
-        return emotionality;
-    }
-
-    public double getExtraversion() {
-        return extraversion;
-    }
-
-    public double getAgreeableness() {
-        return agreeableness;
-    }
-
-    public double getConscientiousness() {
-        return conscientiousness;
-    }
-
-    public double getOpenness() {
-        return openness;
     }
 
     private static double validate(String dimensionName, double value) {
