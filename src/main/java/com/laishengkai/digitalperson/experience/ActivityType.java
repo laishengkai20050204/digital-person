8dcc83645a81b76d2ca72dc5ab266c3a2ed6ef99
laishengkai20050204 <48379994+laishengkai20050204@users.noreply.github.com>
2026-07-21 11:47:14 +0800

    Model activities by concurrency channel

--- src/main/java/com/laishengkai/digitalperson/experience/ActivityType.java
+++ src/main/java/com/laishengkai/digitalperson/experience/ActivityType.java
@@ -1,35 +1,37 @@
 package com.laishengkai.digitalperson.experience;
 
-import lombok.Getter;
+import java.util.Objects;
 
-@Getter
 public enum ActivityType {
 
-    STUDY(ActivityMode.EXCLUSIVE),
-    WORK(ActivityMode.EXCLUSIVE),
-    EAT(ActivityMode.EXCLUSIVE),
-    SLEEP(ActivityMode.EXCLUSIVE),
-    REST(ActivityMode.EXCLUSIVE),
-    TRAVEL(ActivityMode.EXCLUSIVE),
-    EXERCISE(ActivityMode.EXCLUSIVE),
-    SOCIAL(ActivityMode.EXCLUSIVE),
-    ENTERTAINMENT(ActivityMode.EXCLUSIVE),
-    SHOPPING(ActivityMode.EXCLUSIVE),
-    CHAT(ActivityMode.CONCURRENT),
-    LISTEN_MUSIC(ActivityMode.CONCURRENT),
-    OTHER(ActivityMode.EXCLUSIVE);
+    STUDY(ActivityChannel.PRIMARY),
+    WORK(ActivityChannel.PRIMARY),
+    EAT(ActivityChannel.PRIMARY),
+    SLEEP(ActivityChannel.PRIMARY),
+    REST(ActivityChannel.PRIMARY),
+    TRAVEL(ActivityChannel.PRIMARY),
+    EXERCISE(ActivityChannel.PRIMARY),
+    SOCIAL(ActivityChannel.PRIMARY),
+    ENTERTAINMENT(ActivityChannel.PRIMARY),
+    SHOPPING(ActivityChannel.PRIMARY),
 
-    private final ActivityMode mode;
+    CHAT(ActivityChannel.COMMUNICATION),
+    LISTEN_MUSIC(ActivityChannel.AUDIO),
 
-    ActivityType(ActivityMode mode) {
-        this.mode = mode;
+    OTHER(ActivityChannel.PRIMARY);
+
+    private final ActivityChannel channel;
+
+    ActivityType(ActivityChannel channel) {
+        this.channel = Objects.requireNonNull(channel, "channel cannot be null");
     }
 
-    public boolean isExclusive() {
-        return mode == ActivityMode.EXCLUSIVE;
+    public ActivityChannel getChannel() {
+        return channel;
     }
 
-    public boolean isConcurrent() {
-        return mode == ActivityMode.CONCURRENT;
+    public boolean conflictsWith(ActivityType other) {
+        Objects.requireNonNull(other, "other cannot be null");
+        return channel == other.channel;
     }
 }
