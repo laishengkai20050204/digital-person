c57e311d4f05cdac068f95db04570aec0dd626da
laishengkai20050204 <48379994+laishengkai20050204@users.noreply.github.com>
2026-07-21 10:42:34 +0800

    Add activity types for person events

---
+++ src/main/java/com/laishengkai/digitalperson/experience/ActivityType.java
@@ -0,0 +1,16 @@
+package com.laishengkai.digitalperson.experience;
+
+public enum ActivityType {
+    STUDY,
+    WORK,
+    EAT,
+    SLEEP,
+    REST,
+    TRAVEL,
+    EXERCISE,
+    SOCIAL,
+    ENTERTAINMENT,
+    SHOPPING,
+    CHAT,
+    OTHER
+}
