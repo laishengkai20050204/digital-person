76d20372d0b7970a940d9f0d3f6de2666f8836e5
laishengkai20050204 <48379994+laishengkai20050204@users.noreply.github.com>
2026-07-22 11:54:53 +0800

    Add memory retrieval contracts

---
+++ src/main/java/com/laishengkai/digitalperson/memory/MemorySection.java
@@ -0,0 +1,15 @@
+package com.laishengkai.digitalperson.memory;
+
+/** Stable memory partitions owned by the digital-person domain. */
+public enum MemorySection {
+    IDENTITY,
+    RELATIONSHIP,
+    PREFERENCE,
+    GOAL,
+    PLAN,
+    COMMITMENT,
+    EPISODIC,
+    USER_PROFILE,
+    ROUTINE,
+    CONVERSATION_SUMMARY
+}
