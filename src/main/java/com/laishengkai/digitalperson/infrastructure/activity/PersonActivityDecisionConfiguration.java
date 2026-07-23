38671c89277ce7c559554ba721bb1bbc5fab0950
laishengkai20050204 <48379994+laishengkai20050204@users.noreply.github.com>
2026-07-23 18:40:58 +0800

    Remove order-sensitive activity service condition

--- src/main/java/com/laishengkai/digitalperson/infrastructure/activity/PersonActivityDecisionConfiguration.java
+++ src/main/java/com/laishengkai/digitalperson/infrastructure/activity/PersonActivityDecisionConfiguration.java
@@ -3,14 +3,8 @@ package com.laishengkai.digitalperson.infrastructure.activity;
 import com.laishengkai.digitalperson.activity.PersonActivityDecisionModel;
 import com.laishengkai.digitalperson.application.DefaultPersonActivityDecisionContextAssembler;
 import com.laishengkai.digitalperson.application.PersonActivityDecisionContextAssembler;
-import com.laishengkai.digitalperson.application.PersonActivityDecisionService;
 import com.laishengkai.digitalperson.application.PersonModelContextAssembler;
-import com.laishengkai.digitalperson.application.StateEvaluationContextAssembler;
 import com.laishengkai.digitalperson.dialogue.LanguageModelGateway;
-import com.laishengkai.digitalperson.person.PersonRepository;
-import com.laishengkai.digitalperson.state.EventStateImpactEvaluator;
-import com.laishengkai.digitalperson.state.StateUpdater;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -39,30 +33,5 @@ public class PersonActivityDecisionConfiguration {
             PersonModelContextAssembler commonAssembler
     ) {
         return new DefaultPersonActivityDecisionContextAssembler(commonAssembler);
-    }
-
-    @Bean
-    @ConditionalOnBean({
-            PersonRepository.class,
-            PersonActivityDecisionModel.class,
-            EventStateImpactEvaluator.class
-    })
-    @ConditionalOnMissingBean(PersonActivityDecisionService.class)
-    PersonActivityDecisionService personActivityDecisionService(
-            PersonRepository personRepository,
-            StateUpdater stateUpdater,
-            PersonActivityDecisionModel activityDecisionModel,
-            PersonActivityDecisionContextAssembler activityContextAssembler,
-            EventStateImpactEvaluator effectEvaluator,
-            StateEvaluationContextAssembler effectContextAssembler
-    ) {
-        return new PersonActivityDecisionService(
-                personRepository,
-                stateUpdater,
-                activityDecisionModel,
-                activityContextAssembler,
-                effectEvaluator,
-                effectContextAssembler
-        );
     }
 }
