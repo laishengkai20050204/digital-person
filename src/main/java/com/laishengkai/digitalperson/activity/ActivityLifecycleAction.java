b3526d44b825373f4556d54670f504f09a31d9a8
laishengkai20050204 <48379994+laishengkai20050204@users.noreply.github.com>
2026-07-23 17:03:13 +0800

    Add activity lifecycle action

---
+++ src/main/java/com/laishengkai/digitalperson/activity/ActivityLifecycleAction.java
@@ -0,0 +1,7 @@
+package com.laishengkai.digitalperson.activity;
+
+/** Type of one proposed digital-person event lifecycle mutation. */
+public enum ActivityLifecycleAction {
+    START,
+    FINISH
+}
